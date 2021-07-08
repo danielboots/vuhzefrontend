b86c0c532e2a178a488ba4e75b026dee78efeda9
danielboots <you@example.com>
2021-07-08 17:26:08 +0100

    debug post.js

--- pages/Post/index.js
+++ pages/Post/index.js
@@ -1,54 +1,5 @@
-import { sanityClient } from "../../sanity";
-import React, { useState, useEffect } from "react";
-import Layout from "@/components/Layout";
-import Post from "@/components/Post";
-import Info from "@/components/Info";
-
-const post = ({ post }) => {
-  const [postData, setPostData] = useState(null);
-
-  console.log(postData);
-
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `*[_type == 'post']{
-          title,
-          slug,
-          body,
-          mainImage {
-              asset-> {
-                  _id,
-                  url
-              },
-              alt
-      },
-      
-    }`
-      )
-      .then((data) => setPostData(data))
-      .catch(console.error);
-  }, []);
-
-  return (
-    <Layout title="posts | EDM" description="posts | EDM Music posts">
-      <div className=" p-4 container mx-auto font-body ">
-        <h1 className="flex justify-center text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl  ">
-          News
-        </h1>
-
-        <div className=" m-2 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-          {postData &&
-            postData.map((post) => (
-              <div>
-                <Post key={post.id} post={post} />
-              </div>
-            ))}
-        </div>
-      </div>
-      <Info />
-    </Layout>
-  );
+const index = () => {
+  return <div>test</div>;
 };
 
-export default post;
+export default index;
